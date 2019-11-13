1631761a4b553295b5a64204c47f0606ba51c03c
Matthew Ream <matthewtimothyream@gmail.com>
2019-11-13 10:45:07 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { :minimum => 250 }
+    validates :summary, length: { :maximum => 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
+    validate :clickbait
+
+
+        def clickbait
+            titles = [/Secret/, /Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/]
+            if titles.none? {|phrase| phrase.match(title)}
+                self.errors[:title] << "We are only allowed to have titles that include Will Not Believe, Secret, Top [number], or Guess!"
+            end  
+        end
 end
